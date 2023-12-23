16a4e5ea2f12d31f8d6b2f002ffe8688ef98af54
Prince-Foreantech <prince@foreantech.com>
2023-12-23 14:11:14 +0530

    new

--- asvs_web/urls.py
+++ asvs_web/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path("",include("homepage.urls")),
     path("",include("volunteer.urls")),
     path("",include("payment.urls"))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
